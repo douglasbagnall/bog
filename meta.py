d1dc54d714cf06b104d3abafcc09843d3f4ba767
Douglas Bagnall <douglas@halo.gen.nz>
2016-04-10 22:55:16 +1200

    fix write_results

--- meta.py
+++ meta.py
@@ -29,5 +29,40 @@ def read_lang_info(d, lang):
     return dirs
 
 
-def write_results(d, lang, results):
-    pass
+def makepath(fragments):
+    fn = os.path.join(*fragments)
+    d = os.path.dirname(fn)
+    try:
+        os.makedirs(d)
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
+    return fn
+
+
+def write_clusters_json(clusters, *dirpath):
+    json_clusters = []
+    for cluster in clusters:
+        json_clusters.append([{'document': x} for x in cluster])
+
+    fn = makepath(dirpath + ("clustering.json",))
+    f = open(fn, 'w')
+    json.dump(f, json_clusters, indent=4)
+    f.close()
+
+
+def write_attractions_json(attractions, *dirpath):
+    json_pairs = []
+    for a, b, p in attractions:
+        json_pairs.append({'document1': a,
+                           'document2': b,
+                           'score': p})
+    fn = makepath(dirpath + ("ranking.json",))
+    f = open(fn, 'w')
+    json.dump(f, json_pairs, indent=4)
+    f.close()
+
+
+def write_results(d, lang, title, affinities, clusters):
+    write_clusters_json(d, title, lang)
+    write_attractions_json(d, title, lang)
