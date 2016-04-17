ec73eb9ad86c97c9e079d44485c36acb95110cae
Douglas Bagnall <douglas@halo.gen.nz>
2016-04-18 00:22:45 +1200

    eval-lang: use colour output

--- colour.py
+++ colour.py
@@ -17,44 +17,8 @@ WHITE       = "\033[01;37m"
 
 REV_RED     = "\033[01;41m"
 
-_FOREGROUND = "\033[38;5;%sm"
-_BACKGROUND = "\033[48;5;%sm"
-
-_spectrum = (range(160, 196, 6) +
-             range(226, 190, -6) +
-             range(124, 128, 1) +
-             range(128, 164, 6) +
-             range(122, 90, -6) +
-             range(91, 88, -1) +
-             range(161, 166, 1) +
-             range(201, 196, -1) +
-             range(201, 196, -1) +
-             range(130, 160, 6) +
-             range(118, 88, -6))
-
-SPECTRUM = [_FOREGROUND % x for x in _spectrum]
-BACKGROUND_SPECTRUM = [_BACKGROUND % x for x in _spectrum]
-
-
-def get_namespace(use_colour='auto'):
-    import sys
-    class EmptyThing(object):
-        pass
-    ns = EmptyThing()
-
-    if use_colour.lower() in ('yes', 'true', 'y', 'always'):
-        c = True
-    elif use_colour.lower() in ('no', 'false', 'n', 'never'):
-        c = False
-    else:
-        c = sys.stdout.isatty()
-
-    for k, v in globals().items():
-        if k.isupper():
-            setattr(ns, k, (v if c else ''))
-
-    return ns
-
+foreground = "\033[38;5;%sm".__mod__
+background = "\033[48;5;%sm".__mod__
 
 def _gen_print_functions():
     for c in (
