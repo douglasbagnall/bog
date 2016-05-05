936da8d1c7dd7f585b9a45d016e8b1e94aa5ce99
Douglas Bagnall <douglas@halo.gen.nz>
2016-05-05 19:59:37 +1200

    munge: redo cluster-aware to be lazier and better
    Only the actually used scores are calculated, and there is no cut-off.

--- munge.py
+++ munge.py
@@ -127,7 +127,86 @@ def array_to_link_pairs(a, names, include_self=False):
     return links
 
 
-# Connecting
+def add_to_cluster(p, linkmap, p2, scores):
+    #print "adding %s to %s" % (p, c)
+    c = linkmap[p2]
+    s, x = c
+    if p not in x:
+        for y in x:
+            s += scores[(p, y)]
+        x.add(p)
+        c[0] = s
+    linkmap[p] = c
+    n = len(x)
+    scale = 2.0 / ((n + 2) * (n + 1))
+    return s * scale
+
+
+def array_to_link_pairs_cluster_aware(a, names, power=1.0):
+    """make a sorted list of links from the top triangle of an array."""
+    links = array_to_link_pairs(a, names, True)
+    pairs = [(s ** power, p) for s, p in links if p[0] != p[1]]
+    pairs.sort(reverse=True)
+    scores = {}
+    for s, p in pairs:
+        scores[p] = s
+        scores[(p[1], p[0])] = s
+
+    links_out = [(s, p) for s, p in links if p[0] == p[1]]
+
+    linkmap = {}
+    inv_power = 1.0 / power
+    # how many candidates to consider
+    max_n = len(names) * len(names)
+
+    for i, pair in enumerate(pairs[:max_n]):
+        score, link = pair
+        p1, p2 = link
+        if p1 in linkmap and p2 in linkmap:
+            c1 = linkmap[p1]
+            c2 = linkmap[p2]
+            if c1 is not c2:
+                # [score, set]
+                s1, x1 = c1
+                s2, x2 = c2
+                for x in x2:
+                    linkmap[x] = c1
+                    for y in x1:
+                        s1 += scores[(x, y)]
+
+                x1.update(x2)
+                c1[0] = s1 + s2
+
+            n = len(c1[1])
+            #    scale == 1 / (n! / (r! * (n-r)!)
+            #          == r! * (n-r)! / n!, and here r == 2
+            #          ==  2 / (n * (n-1))
+            scale = 2.0 / ((n + 2) * (n + 1))
+            s = (c1[0] * scale) ** inv_power
+            links_out.append((s, link))
+
+        elif p1 in linkmap:
+            s = add_to_cluster(p2, linkmap, p1, scores)
+            links_out.append((s ** inv_power, link))
+
+
+        elif p2 in linkmap:
+            s = add_to_cluster(p1, linkmap, p2, scores)
+            links_out.append((s ** inv_power, link))
+
+        else :
+            c = [score, set(link)]
+            linkmap[p1] = c
+            linkmap[p2] = c
+            links_out.append((score ** inv_power, link))
+    links_out.sort(reverse=True)
+    return links_out
+
+
+def cluster_aware_matrix(data, names, power):
+    links = array_to_link_pairs_cluster_aware(data, names, power)
+    return links_to_matrix(links, names)
+
 
 def cluster_aware_scoring(links, names, depth=3, power=1):
     doc_indices = {d: i for i, d in enumerate(names)}
@@ -184,7 +263,7 @@ def links_to_matrix(links, names):
     return a
 
 
-def cluster_aware_matrix(data, names, power):
+def old_cluster_aware_matrix(data, names, power):
     links = array_to_link_pairs(data, names)
     links = cluster_aware_scoring(links, names, power=power)
     return links_to_matrix(links, names)
